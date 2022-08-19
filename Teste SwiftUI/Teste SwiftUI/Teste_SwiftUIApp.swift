b8383ba22929f615487dc655e68c68637dd28a11
Lucca-Lopes <luccavlopes@hotmail.com>
2022-08-19 15:45:14 -0300

    iniciando o git

---
+++ Teste SwiftUI/Teste SwiftUI/Teste_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  Teste_SwiftUIApp.swift
+//  Teste SwiftUI
+//
+//  Created by Lucca Lopes on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Teste_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
