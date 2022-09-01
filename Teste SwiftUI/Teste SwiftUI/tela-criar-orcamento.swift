be127dd962a8b4ae810449d55ff12fdc6add7d07
Lucca-Lopes <luccavlopes@hotmail.com>
2022-09-01 18:02:45 -0300

    amudou nada desde o ultimo commit

---
+++ Teste SwiftUI/Teste SwiftUI/tela-criar-orcamento.swift
@@ -0,0 +1,20 @@
+//
+//  tela-criar-orcamento.swift
+//  Teste SwiftUI
+//
+//  Created by Lucca Lopes on 01/09/22.
+//
+
+import SwiftUI
+
+struct tela_criar_orcamento: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct tela_criar_orcamento_Previews: PreviewProvider {
+    static var previews: some View {
+        tela_criar_orcamento()
+    }
+}
