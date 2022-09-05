63a176dd769b3cea87a1f75dc3a1fab8136e134c
Guilborges <Lozanoguilherme@gmail.com>
2022-09-05 18:03:26 -0300

    TelaInicial Melhorada

--- Teste SwiftUI/Teste SwiftUI/TelaInicialView.swift
+++ Teste SwiftUI/Teste SwiftUI/TelaInicialView.swift
@@ -15,21 +15,7 @@ struct TelaInicialView: View {
     @State private var rowHeight: CGFloat = 40
     @State private var headerHeight: CGFloat = 60
     
-    struct TodosBotoes: Identifiable {
-        var id: UUID
-        
-        let name: String
-    }
-    
-    let botoes: [TodosBotoes] = [
-        TodosBotoes(id: UUID(), name: "Large Title"),
-        TodosBotoes(id: UUID(), name: "Title"),
-        TodosBotoes(id: UUID(), name: "Headline"),
-        TodosBotoes(id: UUID(), name: "Body"),
-    ]
-    
     var body: some View {
-        
         NavigationView{
             VStack {
         
@@ -50,144 +36,67 @@ struct TelaInicialView: View {
                 Form{
                     
                     Section{
-//                        criaBotao(titulo: "Criar orçamento")
-                        
-                        NavigationLink {
-                            ContentView()
-                        }
-                    label: {
-                        Text("Criar Orçamento")
-                            .foregroundColor(.black)
-                            .bold()
-                            .padding()
-
-                    }
+                        criaBotao(titulo: "Criar orçamento")
                     }
                     
                     Section{
-                        
-                        NavigationLink {
-                            ContentView()
-                        }
-                    label: {
-                        Text("Meus Orçamentos")
-                            .foregroundColor(.black)
-                            .bold()
-                            .padding()
-                        
-                    }
+                      criaBotao(titulo: "Meus orçamentos")
                     }
                     
                     Section{
-                        
-                        NavigationLink {
-                            ContentView()
-                        }
-                    label: {
-                        Text("Calcular valor da hora")
-                            .foregroundColor(.black)
-                            .bold()
-                            .padding()
-                        
-                    }
+                        criaBotao(titulo: "Calcular valor da hora")
                     }
                     
                     Section{
-                        
-                        NavigationLink {
-                            ContentView()
-                        }
-                    label: {
-                        Text("Despesas")
-                            .foregroundColor(.black)
-                            .bold()
-                            .padding()
-                        
-                    }
+                        criaBotao(titulo: "Despesas")
                     }
                     
                     Section{
-                        NavigationLink {
-                            ContentView()
-                        }
-                    label: {
-                        Text("Gastos")
-                            .foregroundColor(.black)
-                            .bold()
-                            .padding()
-                    }
+                        criaBotao(titulo: "Gastos")
                     }
                 }
+                .cornerRadius(40)
             }
             .navigationTitle("Nome do app")
         }
         
     }
 
-//    func criaBotao(titulo: String) {
-//        NavigationLink {
-//            switch titulo{
-//            case "Criar orçamento":
-//                ContentView()
-//
-//            case "Meus orçamentos":
-//                ContentView()
-//
-//            case "Calcular o valor da hora":
-//                ContentView()
-//
-//            case "Despesas":
-//                ContentView()
-//
-//            case "Gastos":
-//                ContentView()
-//
-//            default:
-//                ContentView()
-//            }
-//        }
-//        label: {
-//            Text("Criar Orçamento")
-//                .foregroundColor(.black)
-//                .bold()
-//            .padding()
-//        }
-//    }
+    func criaBotao(titulo: String) -> some View{
+        let destino = NavigationLink {
+            switch titulo{
+            case "Criar orçamento":
+                ContentView()
+
+            case "Meus orçamentos":
+                ContentView()
+
+            case "Calcular o valor da hora":
+                ContentView()
+
+            case "Despesas":
+                ContentView()
+
+            case "Gastos":
+                ContentView()
+
+            default:
+                ContentView()
+            }
+        }
+        label: {
+            Text("Criar Orçamento")
+                .foregroundColor(.black)
+                .bold()
+            .padding()
+        }
+        return destino
+    }
 }
 
 //    RoundedRectangle(cornerRadius: 40, style: .continuous)
 //        .foregroundColor(Color.gray)
 //        .position(x: screenWidth * 0.5, y: screenHeight * 0.5)
-
-//        NavigationView{
-//            ZStack{
-//                ForEach(botoes, id: \.name) { botoes in
-//                    Text(botoes.name)
-//                    NavigationLink(destination: ContentView()) {
-//                        Image(systemName: "info.circle")
-//                        Text("Texto")
-//                Text(" Valor da hora de trabalho:")
-//                   .bold()
-//                   .font(.system(size: 20))
-//                   .position(x: screenWidth * 0.33, y: screenHeight * 0.025)
-//               Text("R$20,13")
-//                   .bold()
-//                   .font(.system(size: 25))
-//
-//                   .position(x: screenWidth * 0.16, y: screenHeight * 0.06)
-//
-//
-//
-
-
-
-
-
-//        }
-
-//
-//        }
-//    }
 
 //struct TelaInicialView_Previews: PreviewProvider {
 //    static var previews: some View {
