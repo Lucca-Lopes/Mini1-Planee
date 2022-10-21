d844ec0af6f1bcc437a99547915b88fe4f9946ed
RudrigoPereira <45149332+RudrigoPereira@users.noreply.github.com>
2022-10-20 22:40:38 -0300

    Tela valor da hora de trabalho

--- Teste SwiftUI/Teste SwiftUI/TelaValorHdT.swift
+++ Teste SwiftUI/Teste SwiftUI/TelaValorHdT.swift
@@ -17,10 +17,13 @@ struct TelaValorHdT: View {
     
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         List{
             HStack{
                 Text("Pretensão salarial")
+                    .foregroundColor(colorScheme == .dark ? Color.white : vm.corLight[4])
                 Spacer()
                 TextField("R$ 0,00", value: $valor, formatter: vm.formatacao)
                     .textFieldStyle(.roundedBorder)
@@ -28,8 +31,12 @@ struct TelaValorHdT: View {
                     .multilineTextAlignment(.trailing)
                     .keyboardType(.decimalPad)
             }
+            .listRowBackground(colorScheme == .dark ? vm.corDark[3] : Color.white)
+            .listRowSeparatorTint(colorScheme == .dark ? vm.corDark[4] : vm.corLight[2])
+            
             HStack{
                 Text("Dias de trabalho")
+                    .foregroundColor(colorScheme == .dark ? Color.white : vm.corLight[4])
                 Spacer()
                 TextField("0 dias", value: $dias, formatter: vm.numFormatacao)
                     .textFieldStyle(.roundedBorder)
@@ -37,8 +44,12 @@ struct TelaValorHdT: View {
                     .multilineTextAlignment(.trailing)
                     .keyboardType(.numberPad)
             }
+            .listRowBackground(colorScheme == .dark ? vm.corDark[3] : Color.white)
+            .listRowSeparatorTint(colorScheme == .dark ? vm.corDark[4] : vm.corLight[2])
+            
             HStack{
                 Text("Horas diárias")
+                    .foregroundColor(colorScheme == .dark ? Color.white : vm.corLight[4])
                 Spacer()
                 TextField("0 horas", value: $horasDiarias, formatter: vm.numFormatacao)
                     .textFieldStyle(.roundedBorder)
@@ -46,6 +57,8 @@ struct TelaValorHdT: View {
                     .multilineTextAlignment(.trailing)
                     .keyboardType(.numberPad)
             }
+            .listRowBackground(colorScheme == .dark ? vm.corDark[3] : Color.white)
+            .listRowSeparatorTint(colorScheme == .dark ? vm.corDark[4] : vm.corLight[2])
             
             Section(header: Text(""))
             {
@@ -55,6 +68,8 @@ struct TelaValorHdT: View {
                     Text("R$ " + String(format: "%.2f", locale: Locale(identifier: "br"), vm.valorDaHora[0].valorFinal))
                 }
             }
+            .listRowBackground(colorScheme == .dark ? vm.corDark[3] : Color.white)
+            .foregroundColor(colorScheme == .dark ? Color.white : vm.corLight[4])
         }
         .listStyle(.insetGrouped)
         .toolbar{
